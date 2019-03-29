991db122c0bf6b5b0d865a2199e87c10fcc921ed
xDaile <43553977+xDaile@users.noreply.github.com>
2019-03-29 13:50:41 +0100

    Update xkobyd00_xzelen24.sql

--- proj2/xkobyd00_xzelen24.sql
+++ proj2/xkobyd00_xzelen24.sql
@@ -2,7 +2,7 @@
 -- VUT FIT 2019 IDS
 
 --TO DO
---TIME,
+
 
 --pull datas into table
 
@@ -59,130 +59,129 @@ END;
 
 
 
-create table Zamestnanec 
+CREATE TABLE zamestnanec 
 (
-rodne_cislo char(11) primary key not null,
-meno varchar2(120) not null,
-priezvisko varchar2(120) not null,
-Adresa varchar2(80) not null,
-cisloUctu char(24),
-hodinovaMzda number(4,3),
-pozicia varchar2(50) not null,
-Constraint pozicia_enum CHECK ( pozicia IN('pokladnik','veduci','majitel','vyhodeny'))
+rodneCislo CHAR(11) PRIMARY KEY NOT NULL,
+meno VARCHAR2(120) NOT NULL,
+priezvisko VARCHAR2(120) NOT NULL,
+adresa VARCHAR2(80) NOT NULL,
+cisloUctu CHAR(24),
+hodinovaMzda NUMBER(4,3),
+pozicia VARCHAR2(50) NOT NULL,
+CONSTRAINT poziciaEnum CHECK ( pozicia IN('pokladnik','veduci','majitel','vyhodeny'))
 );
 
 
-create table multikino
+CREATE TABLE multikino
 (
-nameCin varchar2(50) Primary key not null,
-mesto varchar2(180) NOT NULL, 
-ulica varchar2(180),
-zamestnanci char(11) Not null,
-MultikinoSala number not null,
-constraint FK_zamestMultikina FOREIGN KEY 
-    (zamestnanci) REFERENCES Zamestnanec
-    on delete cascade
+nameCin VARCHAR2(50) PRIMARY KEY NOT NULL,
+mesto VARCHAR2(180) NOT NULL, 
+ulica VARCHAR2(180),
+zamestnanci CHAR(11) NOT NULL,
+multikinoSala NUMBER NOT NULL,
+CONSTRAINT FKzamestMultikina FOREIGN KEY 
+    (zamestnanci) REFERENCES zamestnanec
+    ON DELETE CASCADE
 );
 
 
 
-create table zanerFilmu
-(nazov varchar2(20) NOT NULL, 
-popis varchar2(300),
---film number,
-Constraint PK_zanerFilmu PRIMARY KEY (nazov) 
+CREATE TABLE zanerFilmu
+(nazov VARCHAR2(20) NOT NULL, 
+popis VARCHAR2(300),
+--film NUMBER,
+CONSTRAINT PKzanerFilmu PRIMARY KEY (nazov) 
 );
 
 
 
-create table Film
-(FilmID INT GENERATED AS IDENTITY PRIMARY KEY not null,
-nazovFilmu varchar2(20) NOT NULL,
-rok number(4,0),
-klucslova varchar2(200),
-reziser varchar2(70),
-trvanie number(3,1),
-krajinaPovodu varchar2(50),
-vekoveObmedzenie number(2,0) not null,
-FilmZaner varchar2(20),
-FilmProjekcia number
-/*CONSTRAINT FK_Film_zaner FOREIGn KEY
+CREATE TABLE film
+(filmID INT GENERATED AS IDENTITY PRIMARY KEY NOT NULL,
+nazovFilmu VARCHAR2(20) NOT NULL,
+rok NUMBER(4,0),
+klucSlova VARCHAR2(200),
+reziser VARCHAR2(70),
+trvanie NUMBER(3,1),
+krajinaPovodu VARCHAR2(50),
+vekoveObmedzenie NUMBER(2,0) NOT NULL,
+filmProjekcia NUMBER
+/*CONSTRAINT FK_Film_zaner FOREIGN KEY
     (FilmZaner) REFERENCES zanerFilmu*/
 );
 
-create table zanerFilmu_Film
+CREATE TABLE zanerFilmuFilm
 (
-nazovZanru varchar2(20) not null,
-FilmID number,
-CONSTRAINT FK_Film_zaner FOREIGn KEY
+nazovZanru VARCHAR2(20) NOT NULL,
+filmIdentifikator NUMBER,
+CONSTRAINT FKFilm_zaner FOREIGN KEY
     (nazovZanru) REFERENCES zanerFilmu,
-CONSTRAINT FK_Film_ID FOREIGn KEY
-    (FilmID) REFERENCES Film
+CONSTRAINT FKFilm_ID FOREIGN KEY
+    (filmIdentifikator) REFERENCES film
 );
 
 /*
-alter table zanerFilmu
-ADD constraint FK_zanerFilmuFilm
-    Foreign key (film)
+ALTER TABLE zanerFilmu
+ADD CONSTRAINT FK_zanerFilmuFilm
+    FOREIGN KEY (film)
     REFERENCES Film;*/
 
-create table Projekcia 
+CREATE TABLE projekcia 
 (projekciaID INT GENERATED AS IDENTITY PRIMARY KEY,
-titulky char(1),
-jazyk varchar2(80),
-datum date,
-d3 char(1) not null,
-ProjekciaRezervacia number,
-ProjekciaVstupenka number not null
+titulky CHAR(2),
+jazyk VARCHAR2(80),
+datum TIMESTAMP,
+d3 CHAR(1) NOT NULL,
+projekciaRezervacia NUMBER,
+projekciaVstupenka NUMBER NOT NULL
 );
---,casZacatia time - mal by stacit date?
+--,casZacatia time - mal by stacit DATE?
 
-create table PremietaciaSala
-(PremietaciaSalaID INT GENERATED AS IDENTITY PRIMARY KEY,
-kapacita number(4,0),
-projektor varchar2(50) not null,
-SalaProjekcia number,
-SalaSedadlo number not null,
-constraint FK_SalaProjekcia Foreign key
-    (SalaProjekcia) references Projekcia
-    on delete cascade
-);
-
-create table Rezervacia
-(RezervaciaID INT GENERATED AS IDENTITY PRIMARY KEY not null,
-cas Date not null,
-cena Number(4,3) not null
+CREATE TABLE premietaciaSala
+(premietaciaSalaID INT GENERATED AS IDENTITY PRIMARY KEY,
+kapacita NUMBER(4,0),
+projektor VARCHAR2(50) NOT NULL,
+salaProjekcia NUMBER,
+salaSedadlo NUMBER NOT NULL,
+CONSTRAINT FK_SalaProjekcia FOREIGN KEY
+    (salaProjekcia) REFERENCES projekcia
+    ON DELETE CASCADE
 );
 
-create table Sedadlo
-(SedadloID INT GENERATED AS IDENTITY PRIMARY KEY,
-rad number(4,0) not null,
-poradie number(4,0) not null,
-obsadenost char(1) not null,
-SedadloRezervacia number,
-SedadloVstupenka number,
-constraint FK_SedadloRezervacia Foreign key
-    (SedadloRezervacia) references Rezervacia
-    on delete cascade
+CREATE TABLE rezervacia
+(rezervaciaID INT GENERATED AS IDENTITY PRIMARY KEY NOT NULL,
+cas DATE NOT NULL,
+cena NUMBER(4,2) NOT NULL
 );
 
-create table Klient
-(KlientID varchar2(50) Primary Key not null,
-Heslo varchar2(50) not null,
-vek number(3,0) not null,
-status varchar2(10) not null,
-KlientRezervacia number,
-Constraint status_enum CHECK ( status IN('dieùa','ötudent','dÙchodca','dospel˝','invalid')),
-constraint FK_Klient_Rezervacia Foreign key
-    (KlientRezervacia) references Rezervacia
-    on delete cascade
+CREATE TABLE sedadlo
+(sedadloID INT GENERATED AS IDENTITY PRIMARY KEY,
+rad NUMBER(4,0) NOT NULL,
+poradie NUMBER(4,0) NOT NULL,
+obsadenost CHAR(1) NOT NULL,
+sedadloRezervacia NUMBER,
+sedadloVstupenka NUMBER,
+CONSTRAINT FK_SedadloRezervacia FOREIGN KEY
+    (sedadloRezervacia) REFERENCES rezervacia
+    ON DELETE CASCADE
 );
 
-create table Vstupenka
-(VstupenkaID INT GENERATED AS IDENTITY PRIMARY KEY,
-casPredaja date not null,
-cena number(4,3) not null,
-statusZakaznika varchar2(10) not null
+CREATE TABLE klient
+(klientID VARCHAR2(50) PRIMARY KEY NOT NULL,
+heslo VARCHAR2(50) NOT NULL,
+vek NUMBER(3,0) NOT NULL,
+status VARCHAR2(10) NOT NULL,
+klientRezervacia NUMBER,
+CONSTRAINT statusEnum CHECK ( status IN('die?a','ötudent','dÙchodca','dospel˝','invalid')),
+CONSTRAINT FK_Klient_Rezervacia FOREIGN KEY
+    (klientRezervacia) REFERENCES rezervacia
+    ON DELETE CASCADE
+);
+
+CREATE TABLE vstupenka
+(vstupenkaID INT GENERATED AS IDENTITY PRIMARY KEY,
+casPredaja TIMESTAMP NOT NULL,
+cena NUMBER(4,3) NOT NULL,
+statusZakaznika VARCHAR2(10) NOT NULL
 );
 
 
@@ -190,56 +189,56 @@ statusZakaznika varchar2(10) not null
 
 
     
-alter table Film
-ADD constraint FK_Projekcia_Film
-    Foreign key (FilmProjekcia)
-    REFERENCES Film
-    on delete cascade;
+ALTER TABLE film
+ADD CONSTRAINT FK_Projekcia_Film
+    FOREIGN KEY (filmProjekcia)
+    REFERENCES film
+    ON DELETE CASCADE;
 
-alter table Multikino
-ADD constraint FK_Multikino_Sala
-    Foreign key (MultikinoSala)
-    REFERENCES PremietaciaSala
-    on delete cascade;
+ALTER TABLE multikino
+ADD CONSTRAINT FK_Multikino_Sala
+    FOREIGN KEY (multikinoSala)
+    REFERENCES premietaciaSala
+    ON DELETE CASCADE;
 
-alter table Projekcia
-ADD constraint FK_Projekcia_Rezervacia
-    Foreign key (ProjekciaRezervacia)
-    REFERENCES Projekcia
-    on delete cascade;
+ALTER TABLE projekcia
+ADD CONSTRAINT FK_Projekcia_Rezervacia
+    FOREIGN KEY (projekciaRezervacia)
+    REFERENCES projekcia
+    ON DELETE CASCADE;
 
-alter table Projekcia
-ADD constraint FK_Projekcia_Vstupenka
-    Foreign key (ProjekciaVstupenka)
-    REFERENCES Vstupenka; 
-    --vstupenky po zmazani projekcie ostanu kvÙli vr·teniu peÚazÌ    
+ALTER TABLE projekcia
+ADD CONSTRAINT FK_Projekcia_Vstupenka
+    FOREIGN KEY (projekciaVstupenka)
+    REFERENCES vstupenka; 
+    --vstupenky po zmazani projekcie ostanu kvÙli vr·teniu peoazÌ    
          
-alter table PremietaciaSala
-ADD constraint FK_Sala_Sedadlo
-    Foreign key (SalaSedadlo)
-    references Sedadlo
-    on delete cascade;
+ALTER TABLE premietaciaSala
+ADD CONSTRAINT FK_Sala_Sedadlo
+    FOREIGN KEY (salaSedadlo)
+    REFERENCES sedadlo
+    ON DELETE CASCADE;
 
-alter table Sedadlo
-ADD constraint FK_Vstupenka_Sedadlo
-    Foreign key (SedadloVstupenka)
-    references Vstupenka;
+ALTER TABLE sedadlo
+ADD CONSTRAINT FK_Vstupenka_Sedadlo
+    FOREIGN KEY (sedadloVstupenka)
+    REFERENCES vstupenka;
 
 
 ----------------------------INSERTION OF THE DATA STARTS HERE-----
 
 -----------------------------------INSERTION ENDS HERE------------
---UrËenie spravcu databazy 
-GRANT ALL ON Zamestnanec TO xkobyd00;
-GRANT ALL ON Multikino TO xkobyd00;
-GRANT ALL ON Rezervacia TO xkobyd00;
-GRANT ALL ON ZanerFilmu TO xkobyd00;
-GRANT ALL ON Film TO xkobyd00;
-GRANT ALL ON Sedadlo TO xkobyd00;
-GRANT ALL ON Vstupenka TO xkobyd00;
-GRANT ALL ON PremietaciaSala TO xkobyd00;
-GRANT ALL ON Projekcia TO xkobyd00;
-GRANT ALL ON Klient TO xkobyd00;
+--Ureenie spravcu databazy 
+GRANT ALL ON zamestnanec TO xkobyd00;
+GRANT ALL ON multikino TO xkobyd00;
+GRANT ALL ON rezervacia TO xkobyd00;
+GRANT ALL ON zanerFilmu TO xkobyd00;
+GRANT ALL ON film TO xkobyd00;
+GRANT ALL ON sedadlo TO xkobyd00;
+GRANT ALL ON vstupenka TO xkobyd00;
+GRANT ALL ON premietaciaSala TO xkobyd00;
+GRANT ALL ON projekcia TO xkobyd00;
+GRANT ALL ON klient TO xkobyd00;
 
 COMMIT;
  
